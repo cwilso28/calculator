5c8ea94f84ce8665d17058b1ee6edb9947c13a33
Chris <47066922+cwilso28@users.noreply.github.com>
2025-01-07 20:54:32 -0600

    Test the math functions
    The functions need to be tested to determine if they are operating
    correctly

--- javascript.js
+++ javascript.js
@@ -14,3 +14,12 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+// Test the math functions
+// console.log(add(3, 5))
+// console.log(subtract(4, 2))
+// console.log(subtract(4, 6))
+// console.log(multiply(4,5))
+// console.log(multiply(-2,4))
+// console.log(divide(1, 4))
+// console.log(divide(1, 0))
